598a0a0e32b53a704112878f96bc1cab1f67d7d8
Ball Man <joshua.christensen13@gmail.com>
2018-12-11 21:07:42 -0700

    started contact info page

---
+++ contact.js
@@ -0,0 +1,27 @@
+$("#add-contact").click(function(event) {
+
+    event.preventDefault();
+    
+    var firstName = $("#first-name").val().trim();
+    var lastName = $("#last-name").val().trim();
+    var email = $("#email").val().trim();
+    var comments = $("#text-area").val().trim();
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(comments);
+
+
+})
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBtwbKPk-wwhdgUIVbql_WMwiXXudffNyk",
+    authDomain: "bummerpassword.firebaseapp.com",
+    databaseURL: "https://bummerpassword.firebaseio.com",
+    projectId: "bummerpassword",
+    storageBucket: "bummerpassword.appspot.com",
+    messagingSenderId: "6699316700"
+  };
+  firebase.initializeApp(config);
